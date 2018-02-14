a458c7198738bdb8c082cfae66cffdf54905f40d
PASIONA\caranda <caranda@pasiona.com>
2018-02-14 17:43:11 +0100

    finished 125, start in 126

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import {BrowserRouter, Route} from 'react-router-dom'
+import reducers from './reducers'
+import promise from 'redux-promise'
+import PostIndex from './components/post-index'
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex}/>
+      </div>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
