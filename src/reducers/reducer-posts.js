fdb4676a78c6dda546319758232af2c41ecb9c8d
kowalski <kristian.aranda@gmail.com>
2018-02-15 18:49:43 +0100

    finished blog, continue in bonus: Dynamic forms

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,11 +1,15 @@
-import {FETCH_POSTS, FETCH_POST} from '../actions'
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions'
 import _ from 'lodash'
 
 
 
 export default function(state = {}, action) {
     switch(action.type) {
+        case DELETE_POST:
+        //if the state object has the key of the deleted object, just drop it
+            return _.omit(state, actio.payload)
         case FETCH_POSTS:
+        //convert array to object with id
             return _.mapKeys(action.payload.data, 'id')
         case FETCH_POST:
             //const post = action.payload.data ES5
